b3eb578dd2ee8dc5833eea4ddc0ef848c04ff511
Roznin Ferdawsi <roznin@bracits.com>
2023-03-22 23:51:24 +0600

    initial commit

---
+++ seleniumTest/src/test/java/selenium/Firefoxbrowser.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firefoxbrowser {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+
+        driver.quit();
+
+    }
+}
